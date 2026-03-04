93095f7d0db82d1cbda6f2cbc8a019e8255664fb
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-04 10:26:35 -0500

    commit - Cliente .

--- src/main/java/com/clientes/model/Cliente.java
+++ src/main/java/com/clientes/model/Cliente.java
@@ -1,4 +1,33 @@
 package com.clientes.model;
 
 public class Cliente {
-}
+
+    private int id;
+    private String nombre;
+    private int edad;
+    private String ciudad;
+    private String tipoCliente;
+
+    public Cliente(String nombre, int edad, String ciudad, String tipoCliente) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.ciudad = ciudad;
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+}   
