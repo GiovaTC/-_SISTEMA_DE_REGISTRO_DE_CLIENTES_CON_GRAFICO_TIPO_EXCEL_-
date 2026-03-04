ae87ac1aa53158648e5e39d7f0b7336ec3f087f4
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-04 15:30:39 -0500

    commit - Main .

--- src/main/java/com/clientes/ui/GraficoClientes.java
+++ src/main/java/com/clientes/ui/GraficoClientes.java
@@ -1,4 +1,42 @@
 package com.clientes.ui;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+import com.clientes.service.ClienteService;
+
+import java.util.Map;
+
 public class GraficoClientes {
+
+    public GraficoClientes() {
+
+        try {
+
+            ClienteService service = new ClienteService();
+            Map<String, Integer> datos = service.obtenerEstadisticas();
+
+            DefaultPieDataset dataset = new DefaultPieDataset();
+
+            for (String tipo : datos.keySet()) {
+                dataset.setValue(tipo, datos.get(tipo));
+            }
+
+            JFreeChart chart = ChartFactory.createPieChart(
+                    "DISTRIBUCION DE CLIENTES",
+                    dataset,
+                    true,
+                    true,
+                    false
+            );
+
+            ChartFrame frame = new ChartFrame("GRAFICO CLIENTES", chart);
+            frame.setSize(600, 400);
+            frame.setVisible(true);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
